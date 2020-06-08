f37ed3e36208ce3089aeb063566da00efad4381f
Angelina <deleloper-kaczmarek@yandex.ru>
2020-06-08 20:28:49 +0400

    Initial commit

---
+++ app/src/main/java/io/github/kaczmarek/remotekit/ui/MainActivity.kt
@@ -0,0 +1,46 @@
+package io.github.kaczmarek.remotekit.ui
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import io.github.kaczmarek.circledpad.CircleDPad
+import io.github.kaczmarek.circledpad.CircleDPad.Companion.BOTTOM_BUTTON
+import io.github.kaczmarek.circledpad.CircleDPad.Companion.CENTER_BUTTON
+import io.github.kaczmarek.circledpad.CircleDPad.Companion.LEFT_BUTTON
+import io.github.kaczmarek.circledpad.CircleDPad.Companion.RIGHT_BUTTON
+import io.github.kaczmarek.circledpad.CircleDPad.Companion.TOP_BUTTON
+import io.github.kaczmarek.remotekit.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity(), CircleDPad.OnClickCircleDPadListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        cdp_controller.listener = this
+
+        val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        val pairedDevices = mBluetoothAdapter.bondedDevices
+
+        val s: MutableList<String> = ArrayList()
+        for (bt in pairedDevices) s.add(bt.name)
+
+
+    }
+
+    override fun onClickButton(button: Int) {
+        when (button) {
+            BOTTOM_BUTTON -> {}
+            LEFT_BUTTON -> {}
+            TOP_BUTTON -> {}
+            RIGHT_BUTTON -> {}
+            CENTER_BUTTON -> {}
+        }
+    }
+}
